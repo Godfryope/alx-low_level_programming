6f2e05f528e5f7a8314153d57f217cc143b42679
elolengodfrey <elolengodfrey@gmail.com>
2022-09-08 07:27:38 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-/* main - prints the size of various types */
-/* Return: 0 if exited properly, non-zero otherwise */
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * 
+ *
+ *    * Description: 'the program's description'
+ *
+ *     * 
+ *
+ *      * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
