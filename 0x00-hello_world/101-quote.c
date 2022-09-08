6be74d624c3a776458f398fc5e31eff44524dc07
elolengodfrey <elolengodfrey@gmail.com>
2022-09-08 12:48:40 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful						\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
