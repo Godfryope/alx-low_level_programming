279abe413fa3b1476775d9b97032832ccc35180f
elolengodfrey <elolengodfrey@gmail.com>
2022-09-09 18:12:37 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -25,10 +25,10 @@ int main(void)
 		while (j < 58)
 		{
 			putchar(i);									putchar(j);
+			
+			if (i < 56 || j < 57)
 
-													if (i < 56 || j < 57)
-
-													{
+			{
 														putchar(44);
 
 														putchar(32);
