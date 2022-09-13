71c3d6c5302c8b78ad4980d7232ce88a39d187da
elolengodfrey <elolengodfrey@gmail.com>
2022-09-13 03:43:31 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+// C program to demonstrate putchar() method
+
+
+#include <stdio.h>
+
+int main()
+{
+	// Get the character to be written
+	char ch = 'G';
+
+	// Write the Character to stdout
+
+	putchar(ch);
+}
