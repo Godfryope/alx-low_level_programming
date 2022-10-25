07d31935f3c979902d16a3b6d6b9f33c7bc1f0c1
elolengodfrey <elolengodfrey@gmail.com>
2022-10-24 20:12:47 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nnodes++;
+		i++;
 	}
-	return (nnodes);
+	return (i);
 }
